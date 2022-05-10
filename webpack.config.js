cfba5f52f2544e0c53abc5c73a289e74c1515c57
FlatShoes <flatshoes@163.com>
2022-05-10 10:01:10 +0800

    初始化模版

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    noParse: /^(vue|vue-router|vuex|vuex-router-sync)$/,
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+}
