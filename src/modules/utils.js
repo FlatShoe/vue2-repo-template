c8e1d6679e5adfbe9e2805a4b4a8ec108d9c552e
FlatShoes <flatshoes@163.com>
2022-06-01 15:37:35 +0800

    modify

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,9 +1,6 @@
 export const getApiBaseUrl = () => {
   return process.env.API_BASEURL
 }
-export const getUserInfoUrl = () => {
-  return `${getApiBaseUrl()}/getUserInfo`
-}
 export const getUploadURL = () => {
   return `${getApiBaseUrl()}/upload`
 }
