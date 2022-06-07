12da21bcac5dabdc2bcaffb083799311afafc171
FlatShoes <flatshoes@163.com>
2022-06-07 18:38:09 +0800

    引入echart

--- src/components/charts/Chart.js
+++ src/components/charts/Chart.js
@@ -1,7 +1,102 @@
+import _ from 'lodash'
 export default {
   name: 'Chart',
-  props: {},
   render(h) {
-    return h('div', 123)
+    return h(
+      'div',
+      {
+        class: [_.camelCase(this.$options.name || this.$options._componentTag)],
+        style: this.chartStyle
+      },
+      [
+        h('div', {
+          style: this.chartStyle,
+          ref: 'canvas'
+        }),
+        h(
+          'div',
+          {
+            class: 'chart-attachment',
+            style: {display: this.loading || this.empty ? '' : 'none'}
+          },
+          [
+            h('div', {
+              class: 'chart-loading',
+              style: {display: this.loading ? '' : 'none'},
+              directives: [
+                {
+                  name: 'loading',
+                  value: true
+                }
+              ]
+            }),
+            h(
+              'span',
+              {
+                class: 'chart-empty',
+                style: {
+                  display: this.empty && !this.loading ? '' : 'none'
+                }
+              },
+              this.emptyText
+            )
+          ]
+        ),
+        this.$slots.default
+      ]
+    )
+  },
+  props: {
+    width: {type: String, default: 'auto'},
+    height: {type: String, default: '400px'},
+    loading: {type: Boolean, default: false},
+    empty: {type: Boolean, default: false},
+    emptyText: {type: String, default: '暂无数据'},
+    resizeable: {type: Boolean, default: true},
+    resizeDelay: {type: Number, default: 200},
+    option: {type: Object, required: true}
+  },
+  computed: {
+    chartStyle() {
+      return {
+        width: this.width,
+        height: this.height,
+        position: 'relative'
+      }
+    }
+  },
+  methods: {
+    init() {
+      if (this.echarts) return
+      this.echarts = this.$echarts.init(this.$refs.canvas)
+      if (this.resizeable) this.resize()
+    },
+    echartsResize() {
+      this.echarts.resize()
+    },
+    resize() {
+      window.addEventListener('resize', this.resizeHandler)
+    },
+    clear() {
+      if (this.resizeable) window.removeEventListener('resize', this.resizeHandler)
+      this.echarts.dispose()
+    }
+  },
+  watch: {
+    option: {
+      handler(val) {
+        if (val && this.echarts) this.echarts.setOption(val)
+      },
+      deep: true
+    }
+  },
+  created() {
+    this.resizeHandler = _.debounce(this.echartsResize, this.resizeDelay)
+  },
+  mounted() {
+    this.init()
+  },
+  beforeDestroy() {
+    this.clear()
   }
 }
--- src/views/charts-view/ChartsView.vue
+++ src/views/charts-view/ChartsView.vue
@@ -4,7 +4,12 @@
 -->
 <template>
   <div class="charts-view">
-    <chart />
+    <div class="view-bar">
+      <chart height="100%" :loading="barLoading" :option="barOption"></chart>
+    </div>
+    <div class="view-pie">
+      <chart height="100%" :loading="pieLoading" :option="pieOption" />
+    </div>
   </div>
 </template>
 
@@ -14,8 +19,99 @@ export default {
   name: 'ChartsView',
   components: {
     Chart
+  },
+  data() {
+    return {
+      barLoading: false,
+      barOption: {},
+      pieLoading: false,
+      pieOption: {}
+    }
+  },
+  methods: {
+    getBarData() {
+      this.barLoading = true
+      setTimeout(() => {
+        this.barOption = {
+          legend: {
+            data: ['销量']
+          },
+          xAxis: {
+            data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+          },
+          yAxis: {},
+          series: [
+            {
+              name: '销量',
+              type: 'bar',
+              data: [20, 30, 10, 40, 20, 70]
+            }
+          ]
+        }
+        this.barLoading = false
+      }, 2000)
+    },
+    getPieOption() {
+      this.pieLoading = true
+      setTimeout(() => {
+        this.pieOption = {
+          tooltip: {
+            trigger: 'item'
+          },
+          legend: {
+            top: '5%',
+            left: 'center'
+          },
+          series: [
+            {
+              name: 'Access From',
+              type: 'pie',
+              radius: ['40%', '70%'],
+              avoidLabelOverlap: false,
+              itemStyle: {
+                borderRadius: 10,
+                borderColor: '#fff',
+                borderWidth: 2
+              },
+              label: {
+                show: false,
+                position: 'center'
+              },
+              emphasis: {
+                label: {
+                  show: true,
+                  fontSize: '40',
+                  fontWeight: 'bold'
+                }
+              },
+              labelLine: {
+                show: false
+              },
+              data: [
+                {value: 1048, name: 'Search Engine'},
+                {value: 735, name: 'Direct'},
+                {value: 580, name: 'Email'},
+                {value: 484, name: 'Union Ads'},
+                {value: 300, name: 'Video Ads'}
+              ]
+            }
+          ]
+        }
+        this.pieLoading = false
+      }, 1000)
+    }
+  },
+  mounted() {
+    this.getBarData()
+    this.getPieOption()
   }
 }
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.charts-view {
+  & > div {
+    height: 500px;
+  }
+}
+</style>
